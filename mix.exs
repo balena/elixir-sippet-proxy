e8aa115a9b34859798ccf28f56bd0c7ea5659712
G. B. Versiani <guibv@yahoo.com>
2017-05-30 10:51:09 -0300

    Update to Sippet >= 0.6.0

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Sippet.Proxy.Mixfile do
   end
 
   defp deps do
-    [{:sippet, "~> 0.5.8"},
+    [{:sippet, ">= 0.6.0"},
 
      # Docs dependencies
      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
